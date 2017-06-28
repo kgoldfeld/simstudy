15dff4b06d4de8154d24fccc12602b2ef4318264
Keith Goldfeld <keith.goldfeld@nyumc.org>
2017-06-28 15:03:17 -0400

    Fixing ...

--- src/simstudy_init.c
+++ src/simstudy_init.c
@@ -1,5 +1,5 @@
 #include <R.h>
-#include <Rinternals.h>
+#include <Rinternals.h> // for SEXP
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
@@ -11,14 +11,16 @@ Check these declarations against the C/Fortran source code.
 extern SEXP simstudy_vecMultinom(SEXP probsSEXP);
 extern SEXP simstudy_matMultinom(SEXP probmatrixSEXP);
 
-static const R_CallMethodDef CallEntries[] = {
-  {"simstudy_matMultinom", (DL_FUNC) &simstudy_matMultinom, 1},
-  {"simstudy_vecMultinom", (DL_FUNC) &simstudy_vecMultinom, 1},
+#define CALLDEF(name, n)  {#name, (DL_FUNC) &name, n}
+
+static const R_CallMethodDef R_CallDef[] = {
+  CALLDEF(simstudy_matMultinom, 1),
+  CALLDEF(simstudy_vecMultinom, 1),
   {NULL, NULL, 0}
 };
 
-void R_init_simstudy(DllInfo *info)
+void R_init_simstudy(DllInfo *dll)
 {
-  R_registerRoutines(info, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(info, TRUE);
+  R_registerRoutines(dll, NULL, R_CallDef, NULL, NULL);
+  // R_useDynamicSymbols(info, TRUE); // Not in R docs
 }
