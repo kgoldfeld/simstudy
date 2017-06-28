31b76c72c70cee8cd2f038b82d1f855427e4e205
Keith Goldfeld <keith.goldfeld@nyumc.org>
2017-06-28 13:45:32 -0400

    Dynamic?

---
+++ src/init.c
@@ -0,0 +1,8 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_simstudy(DllInfo* info) {
+  R_registerRoutines(info, NULL, NULL, NULL, NULL);
+  R_useDynamicSymbols(info, TRUE);
+}
