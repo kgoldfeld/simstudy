408babd4076827105c2020ba4d6951ae5b6ae2ce
R. Maximilian Bee <r.maximilian.bee@fu-berlin.de>
2019-04-15 16:17:01 +0200

    Fix .genQuantU command to include corMatrix.

--- R/genCorOrdCat.R
+++ R/genCorOrdCat.R
@@ -95,7 +95,7 @@ genCorOrdCat <- function(dtName, idname = "id", adjVar = NULL, baseprobs,
   
   N <- nrow(dtName)
   nq <- nrow(baseprobs)
-  zs <- .genQuantU(nq, N, rho = rho, corstr, corMatrix = NULL)
+  zs <- .genQuantU(nq, N, rho = rho, corstr, corMatrix = corMatrix)
   zs[, logisZ := stats::qlogis(p = zs$Unew)]
   cprop <- t(apply(baseprobs, 1, cumsum))
   quant <- t(apply(cprop, 1, stats::qlogis))
