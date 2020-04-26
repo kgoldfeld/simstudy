c5d2dcdb43e71629f227733f24e4db97e5ad33d7
Jotun <gitlab@krushak.me>
2020-04-26 16:15:15 +0200

    Move small stuff to functions + format.

--- R/int_evalDef.R
+++ R/int_evalDef.R
@@ -7,149 +7,143 @@
 # @param defVars Existing column names
 # @return Nothing is returned if all tests are passed. If a test fails,
 # execution is halted.
-.evalDef <- function(newvar, newform, newdist, variance = 0, link = "identity", defVars) {
-  
-  if(!is.character(newvar) || length(newvar) != 1 || is.na(newvar)){
-    stop("Parameter 'varname' must be single string.", call. = FALSE)
-  }
-
-  if (!newdist %in% .getDists()) {
-    stop(paste0("'", newdist, "' distribution is not a valid option. See ?distributions."), call. = FALSE)
-  }
-
-  if (missing(defVars)) {
-    warning("Argument 'defVars' missing with no default. Was this intentional?")
-    defVars <- ""
-  }
-  
-  if (startsWith(newvar, "..")){
-    stop(
-      paste(
-        "The prefix '..' is reserved to escape variables",
-        "from outside the definition table in formulas."
+.evalDef <-
+  function(newvar,
+           newform,
+           newdist,
+           variance = 0,
+           link = "identity",
+           defVars) {
+    if (!is.character(newvar) || length(newvar) != 1 || is.na(newvar)) {
+      stop("Parameter 'varname' must be single string.", call. = FALSE)
+    }
+    
+    if (!newdist %in% .getDists()) {
+      stop(
+        paste0(
+          "'",
+          newdist,
+          "' distribution is not a valid option. See ?distributions."
+        ),
+        call. = FALSE
       )
-    )}
-  
-  if (!is_valid_variable_name(newvar)) {
-    warning(
-      paste(
-        "Variable name '",
-        newvar,
-        "' is not a valid R variable name,\n",
-        "and will be converted to: '",
-        make.names(newvar),
-        "'."
-      ),
-      call. = FALSE
-    )
-    newvar <- make.names(newvar)
-  }
-  
-  if (newvar %in% defVars) {
-    stop(paste("Variable name '", newvar, "' previously defined."), call. = FALSE)
-  }
-  
- 
-  switch(newdist,
-         
-         binary = {
-           .isValidArithmeticFormula(newform, defVars)
-           .isIdLogit(link)
-         }, 
-         
-         beta = ,
-         
-         binomial = {
-           .isValidArithmeticFormula(newform, defVars)
-           .isValidArithmeticFormula(variance, defVars)
-           .isIdLogit(link)
-         }, 
-         
-         noZeroPoisson = ,
-         
-         poisson = ,
-         
-         exponential = {
-           .isValidArithmeticFormula(newform, defVars)
-           .isIdLog(link)
-         }, 
-
-         gamma = , 
-
-         negBinomial = {
-           .isValidArithmeticFormula(newform, defVars)
-           .isValidArithmeticFormula(variance, defVars)
-           .isIdLog(link)
-         },
-         
-         nonrandom =  .isValidArithmeticFormula(newform, defVars),
-         
-         normal = {
-           .isValidArithmeticFormula(newform, defVars)
-           .isValidArithmeticFormula(variance, defVars)
-         },
-        
-         categorical = .checkCategorical(newform),
-   
-         mixture = {
-           .isValidArithmeticFormula(newform)
-           .checkMixture(newform, defVars)
-         },
-         
-         uniform = .checkUniform(newform),
-         
-         uniformInt = .checkUniformInt(newform),
-         
-         stop("Unkown distribution."))
-  
-  invisible(newvar)
-}
-
-.isIdLogit <- function(link) {
-  if (!link %in% c("identity", "logit"))
-    stop(
-      paste(
-        "Invalid link function: '",
-        link,
-        "', must be 'identity' or 'logit'. See ?distributions"
+    }
+    
+    if (missing(defVars)) {
+      warning("Argument 'defVars' missing with no default. Was this intentional?")
+      defVars <- ""
+    }
+    
+    if (startsWith(newvar, "..")) {
+      stop(
+        paste(
+          "The prefix '..' is reserved to escape variables",
+          "from outside the definition table in formulas."
+        )
       )
-    )
-  invisible(link)
-}
-
-.isIdLog <- function(link) {
-  if (!link %in% c("identity", "log"))
-    stop(
-      paste(
-        "Invalid link function: '",
-        link,
-        "', must be 'identity' or 'logit'. See ?distributions"
+    }
+    
+    if (!is_valid_variable_name(newvar)) {
+      warning(
+        paste(
+          "Variable name '",
+          newvar,
+          "' is not a valid R variable name,\n",
+          "and will be converted to: '",
+          make.names(newvar),
+          "'."
+        ),
+        call. = FALSE
       )
+      newvar <- make.names(newvar)
+    }
+    
+    if (newvar %in% defVars) {
+      stop(paste("Variable name '", newvar, "' previously defined."),
+           call. = FALSE)
+    }
+    
+    
+    switch(
+      newdist,
+      
+      binary = {
+        .isValidArithmeticFormula(newform, defVars)
+        .isIdLogit(link)
+      },
+      
+      beta = ,
+      
+      binomial = {
+        .isValidArithmeticFormula(newform, defVars)
+        .isValidArithmeticFormula(variance, defVars)
+        .isIdLogit(link)
+      },
+      
+      noZeroPoisson = ,
+      
+      poisson = ,
+      
+      exponential = {
+        .isValidArithmeticFormula(newform, defVars)
+        .isIdLog(link)
+      },
+      
+      gamma = ,
+      
+      negBinomial = {
+        .isValidArithmeticFormula(newform, defVars)
+        .isValidArithmeticFormula(variance, defVars)
+        .isIdLog(link)
+      },
+      
+      nonrandom =  .isValidArithmeticFormula(newform, defVars),
+      
+      normal = {
+        .isValidArithmeticFormula(newform, defVars)
+        .isValidArithmeticFormula(variance, defVars)
+      },
+      
+      categorical = .checkCategorical(newform),
+      
+      mixture = {
+        .isValidArithmeticFormula(newform)
+        .checkMixture(newform, defVars)
+      },
+      
+      uniform = .checkUniform(newform),
+      
+      uniformInt = .checkUniformInt(newform),
+      
+      stop("Unkown distribution.")
     )
-  
-  invisible(link)
-}
+    
+    invisible(newvar)
+  }
 
-.checkCategorical <- function(formula){
-  probs <- unlist(strsplit(formula,split=";",fixed = T))
+.checkCategorical <- function(formula) {
+  probs <- .splitFormula(formula)
   probs <- suppressWarnings(as.numeric(probs))
   
-  if(length(probs) < 2 || any(is.na(probs)))
-    stop(paste0("The formula for 'categorical' must contain atleast", 
-                " two numeric probabilities." ))
+  if (length(probs) < 2 || any(is.na(probs)))
+    stop(
+      paste0(
+        "The formula for 'categorical' must contain atleast",
+        " two numeric probabilities."
+      )
+    )
   
   #all.equal allows for insignificant tolerances due to floats
-  if(!isTRUE(all.equal(sum(probs),1)))
+  if (!isTRUE(all.equal(sum(probs), 1)))
     stop("Probabilities must sum to 1. See ?distributions")
   
   invisible(formula)
 }
 
-.checkMixture <- function(formula){
-  
-  formula <- gsub("[[:blank:]]","",formula)
-  var_pr <- strsplit(formula,"+",fixed=T)
-  var_dt <- strsplit(var_pr[[1]],"|",fixed=T)
+.checkMixture <- function(formula) {
+  formula <- .rmWS(formula)
+  var_pr <- strsplit(formula, "+", fixed = T)
+  var_dt <- strsplit(var_pr[[1]], "|", fixed = T)
   
   if (length(unlist(var_dt)) %% 2)
     stop(
@@ -160,23 +154,25 @@
       )
     )
   
-  formDT <- as.data.table(do.call(rbind,var_dt))
-  names(formDT) <- c("vars","probs")
+  formDT <- as.data.table(do.call(rbind, var_dt))
+  names(formDT) <- c("vars", "probs")
   
-  dotProbs <- startsWith(formDT$probs,"..")
-  dotVars <- startsWith(formDT$vars,"..")
-  dotVarArrays <- grepl("\\.\\..+\\[",formDT$vars)
-  dotProbArrays <- grepl("\\.\\..+\\[",formDT$probs)
-  dotProbsNames <- sub("..","",formDT$probs[dotProbs])
-  dotVarNames <- sub("..","",formDT$vars[dotVars & !dotVarArrays])
-  notDotVarProbs <- is.na(suppressWarnings(as.numeric(formDT$probs))) 
+  dotProbs <- startsWith(formDT$probs, "..")
+  dotVars <- startsWith(formDT$vars, "..")
+  dotVarArrays <- .getDotArrs(formDT$vars)
+  dotProbArrays <- .getDotArrs(formDT$probs)
+  dotProbsNames <- .rmDots(formDT$probs[dotProbs])
+  dotVarNames <- .rmDots(formDT$vars[dotVars & !dotVarArrays])
+  notDotVarProbs <-
+    is.na(suppressWarnings(as.numeric(formDT$probs)))
   
   if (any(dotVarArrays) |
       any(notDotVarProbs[!dotProbs])) {
     stop(
       paste0(
         "Invalid variable(s): ",
-        paste0(formDT$probs[(notDotVarProbs & !dotProbs) | dotProbArrays],collapse=", "),
+        paste0(formDT$probs[(notDotVarProbs &
+                               !dotProbs) | dotProbArrays], collapse = ", "),
         "\n",
         "Probabilities can only be numeric or numeric",
         " ..vars (not arrays). See ?distribution"
@@ -188,34 +184,38 @@
   formDT$vars[dotVars & !dotVarArrays] <- mget(dotVarNames)
   formDT$probs <- suppressWarnings(as.numeric(formDT$probs))
   
-  if(any(is.na(formDT$probs)))
-    stop(paste0("Probabilites contain 'NA',",
-                " probably through coercion from String."))
+  if (any(is.na(formDT$probs)))
+    stop(paste0(
+      "Probabilites contain 'NA',",
+      " probably through coercion from String."
+    ))
   
-  if(!isTRUE(all.equal(sum(formDT$probs),1)))
+  if (!isTRUE(all.equal(sum(formDT$probs), 1)))
     stop("Probabilities must sum to 1 and not See ?distributions")
   
   invisible(formula)
 }
 
-.checkUniform <- function(formula){
-  
-  range <- unlist(strsplit(formula,";",fixed = T))
+.checkUniform <- function(formula) {
+  range <- .splitFormula(formula)
   # We test for NAs so coercion warning can be suppressed.
   range <- suppressWarnings(as.numeric(range))
   
-  if(length(range) != 2 || any(is.na(range)))
-    stop(paste(
-      "Formula for unifrom distributions must have",
-      "the format: 'min;max' \nwhere min/max are numeric. See ?distributions" 
-      ))
+  if (length(range) != 2 || any(is.na(range)))
+    stop(
+      paste(
+        "Formula for unifrom distributions must have",
+        "the format: 'min;max' \nwhere min/max are numeric. See ?distributions"
+      )
+    )
   
   r_min <- range[1]
   r_max <- range[2]
   
   if (r_min == r_max) {
     warning(
-      paste0("Formula warning: ",
+      paste0(
+        "Formula warning: ",
         "'min' and 'max' are both: '",
         r_min,
         "'.",
@@ -224,10 +224,10 @@
     )
   }
   
-  if(r_max < r_min)
+  if (r_max < r_min)
     stop("Formula invalid: 'max' < 'min'")
   
-  invisible(c(r_min,r_max))
+  invisible(c(r_min, r_max))
 }
 
 .checkUniformInt <- function(formula) {
@@ -243,28 +243,33 @@
 
 
 .isValidArithmeticFormula <- function(formula, defVars) {
-  
-  if(grepl(";",formula,fixed = T))
+  if (grepl(";", formula, fixed = T))
     stop("';' are not allowed in arithmetic formulas. See ?distribution")
   
   # This only catches gross errors like trailing operators, does not check
   # functionnames etc.
   newExpress <- try(parse(text = formula), silent = TRUE)
-
-  if (.iserror(newExpress)) {
-    stop(paste("Equation: '", formula, "' not in proper form. See ?distributions ."),
-      call. = FALSE
-    )}
   
-  formFuncs <- all.names(newExpress,unique = T)
+  if (.iserror(newExpress)) {
+    stop(paste(
+      "Equation: '",
+      formula,
+      "' not in proper form. See ?distributions ."
+    ),
+    call. = FALSE)
+  }
+  
+  formFuncs <- all.names(newExpress, unique = T)
   formVars <- all.vars(newExpress)
   formFuncs <- formFuncs[!formFuncs %in% formVars]
   
   if (any(startsWith(formFuncs, "..")))
     warning(
-      paste("Functions don't need to be escaped with '..',",
+      paste(
+        "Functions don't need to be escaped with '..',",
         "\nunless this is the real name this will cause an error later.",
-        "\nFunctions:",formFuncs[startsWith(formFuncs, "..")]
+        "\nFunctions:",
+        formFuncs[startsWith(formFuncs, "..")]
       )
     )
   
@@ -273,23 +278,80 @@
   unRefVars <- !inDef & !dotVarsBol
   
   if (any(unRefVars)) {
-    
-    stop(paste("Variable(s) referenced not previously defined:",
-               paste(formVars[unRefVars], collapse = ", ")
-    ), call. = FALSE)
+    stop(paste(
+      "Variable(s) referenced not previously defined:",
+      paste(formVars[unRefVars], collapse = ", ")
+    ),
+    call. = FALSE)
   }
   
-  naFormFuncs <- is.na(mget(formFuncs,ifnotfound = NA,mode = "function",inherits = T))
-  if(any(naFormFuncs))
-    stop(paste("Functions(s) referenced not defined:",
-               paste(formFuncs[naFormFuncs], collapse = ", ")
+  naFormFuncs <-
+    is.na(mget(
+      formFuncs,
+      ifnotfound = NA,
+      mode = "function",
+      inherits = T
+    ))
+  
+  if (any(naFormFuncs))
+    stop(paste(
+      "Functions(s) referenced not defined:",
+      paste(formFuncs[naFormFuncs], collapse = ", ")
     ), call. = FALSE)
   
-  naDotVars <- is.na(mget(sub("..","",formVars[dotVarsBol]),ifnotfound = NA,mode = "numeric",inherits = T))
-  if(any(naDotVars))
-    stop(paste("Escaped variables referenced not defined (or not numeric):",
-               paste(names(naDotVars), collapse = ", ")
-    ), call. = FALSE)
+  naDotVars <-
+    is.na(mget(
+      sub("..", "", formVars[dotVarsBol]),
+      ifnotfound = NA,
+      mode = "numeric",
+      inherits = T
+    ))
+  
+  if (any(naDotVars))
+    stop(paste(
+      "Escaped variables referenced not defined (or not numeric):",
+      paste(names(naDotVars), collapse = ", ")
+    ),
+    call. = FALSE)
   
   invisible(formula)
+}
+
+.isIdLogit <- function(link) {
+  .isLink(link, c("identity", "logit"))
+  invisible(link)
+}
+
+.isIdLog <- function(link) {
+  .isLink(link, c("identity", "log"))
+  invisible(link)
+}
+
+.isLink <- function(link, options) {
+  if (!link %in% options)
+    stop(paste0(
+      "Invalid link function: '",
+      link,
+      "', must be ",
+      paste0("'", options, "'", collapse = " or "),
+      ". See ?distributions"
+    ))
+  
+  invisible(link)
+}
+
+.getDotArrs <- function(names) {
+  grepl("\\.\\..+\\[", names)
+}
+
+.rmDots <- function(names) {
+  sub("..", "", names)
+}
+
+.rmWS <- function(str) {
+  gsub("[[:blank:]]", "", str)
+}
+
+.splitFormula <- function(formula) {
+  unlist(strsplit(formula, ";", fixed = T))
 }
