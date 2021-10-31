211d2c3d878be984fd2e8c917ef00e5f0e244bcf
assignUser <jacob@wujciak.de>
2021-10-31 14:12:38 +0100

    fix hedgehog loading issue

--- tests/testthat/helper-gen_def.R
+++ tests/testthat/helper-gen_def.R
@@ -1,4 +1,4 @@
-library(hedgehog)
+library(hedgehog, pos = 3)
 # Generators:
 #   This file implements helper functions and generators used in property based
 #   testing using the hedgehog package. Generators do not just return one value
