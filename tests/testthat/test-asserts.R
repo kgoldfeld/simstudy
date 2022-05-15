b57f718527c1eb07e112110b724546b89a099aca
elinsooon <max.elinson@gmail.com>
2022-05-15 11:28:02 -0400

    work on assertIntegerOrFactor, work in progress, not finished

--- tests/testthat/test-asserts.R
+++ tests/testthat/test-asserts.R
@@ -93,22 +93,24 @@ test_that("assertFactor works.", {
   ))
 })
 
-# test_that("assertIntegerOrFactor works.", {
-#   expect_error(assertIntegerOrFactor(
-#     a = "two", b = 123.456,
-#     c = as.factor(c(1, 2, 3)),
-#     d = as.factor(1.1), e = as.factor("one")
-#   ),
-#   regexp = "a and b", class = "simstudy::wrongType"
-#   )
-#   
-#   expect_silent(assertIntegerOrFactor(
-#     a = as.factor("two"), b = as.factor(123.456),
-#     c = as.factor(c(1, 2, 3)),
-#     d = as.factor(1.1), e = as.factor("one"),
-#     f = 1, g = c(12, 34, 56)
-#   ))
-# })
+test_that("assertIntegerOrFactor works.", {
+  expect_error(assertIntegerOrFactor(
+    a = "two", b = 123.456,
+    c = as.factor(c(1, 2, 3)),
+    d = as.factor(1.1), e = as.factor("one"), 
+    f = as.factor(c(1, "two", 3))
+  ),
+  regexp = "a and b", class = "simstudy::wrongType"
+  )
+
+  expect_silent(assertIntegerOrFactor(
+    a = as.factor("two"), b = as.factor(123.456),
+    c = as.factor(c(1, 2, 3)),
+    d = as.factor(1.1), e = as.factor("one"),
+    f = 1, g = c(12, 34, 56), 
+    h = as.factor(c(1, "two", 3))
+  ))
+})
 
 test_that("assertValue works.", {
   expect_error(assertValue(a = NULL, b = NA, c = character(0)),
