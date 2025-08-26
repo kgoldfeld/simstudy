726b33b8e6d212b203c9f5324a1509d726ae1f6c
Keith Goldfeld <KSG@180MADPHMLT162.local>
2025-08-26 16:16:43 -0400

    Adding possible fix for CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,15 @@ library(testthat)
 library(hedgehog)
 library(simstudy)
 
-data.table::setDTthreads(2) # added to solve CRAN issue
+# data.table::setDTthreads(2) # added before 2025.08.26 to solve CRAN issue
+
+# Set single thread for CI stability. # Added 2025.08.26 to solve CRAN issue
+data.table::setDTthreads(1)
+# Or detect if in CI and adjust accordingly
+if (Sys.getenv("CI") != "") {
+  data.table::setDTthreads(1)
+} else {
+  data.table::setDTthreads(2)
+}
+
 test_check("simstudy")
