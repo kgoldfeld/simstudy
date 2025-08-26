164f5575780251f365794657c549e802a2b6c91b
Keith Goldfeld <KSG@180MADPHMLT162.local>
2025-08-26 16:29:55 -0400

    Still fixing error with CRAN tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,11 +6,5 @@ library(simstudy)
 
 # Set single thread for CI stability. # Added 2025.08.26 to solve CRAN issue
 data.table::setDTthreads(1)
-# Or detect if in CI and adjust accordingly
-if (Sys.getenv("CI") != "") {
-  data.table::setDTthreads(1)
-} else {
-  data.table::setDTthreads(2)
-}
 
 test_check("simstudy")
