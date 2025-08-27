0862e64e536e346b2df28787d36c6ca781b94c19
Keith Goldfeld <KSG@180madphmlt162.wireless.nyumc.org>
2025-08-27 15:27:15 -0400

    Debugging fix

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,14 +1,9 @@
+data.table::setDTthreads(1)  # Added 2025.08.26 to solve CRAN issue
+
 library(testthat)
 library(simstudy)
-library(hedgehog)
 
 # data.table::setDTthreads(2) # added before 2025.08.26 to solve CRAN issue
-
-# Set single thread for CI stability. # Added 2025.08.26 to solve CRAN issue
-# data.table::setDTthreads(1) # if 2 gives an error.
-
-# Also set it after loading
-data.table::setDTthreads(2)
 
 test_check("simstudy") # original 
 
