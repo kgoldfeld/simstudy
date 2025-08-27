854245da64928eba51fbafd083d2aa4d06a26638
Keith Goldfeld <KSG@180madphmlt162.wireless.nyumc.org>
2025-08-27 15:35:48 -0400

    Debugging CI

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,6 @@
 data.table::setDTthreads(1)  # Added 2025.08.26 to solve CRAN issue
 
 library(testthat)
-library(simstudy)
 
 # data.table::setDTthreads(2) # added before 2025.08.26 to solve CRAN issue
 
